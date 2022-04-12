0122621164d2fd8053f206869c27c782a6de8654
Saúl Páez <57554229+SaulPaez03@users.noreply.github.com>
2022-04-11 21:58:16 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ const { conn } = require("./src/db.js");
 
 // Syncing all the models at once.
 conn.sync().then(() => {
-	server.listen(() => {
+	server.listen(3001:() => {
 		console.log("Server is running"); // eslint-disable-line no-console
 	});
 });
